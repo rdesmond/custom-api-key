32f3d5bac0a53bdc69545161666c22d709dd5db0
Ryan Desmond <radesmond@gmail.com>
2018-04-11 15:03:15 +0800

    pushing class presentation on simple roll-your-own api key auth - pre interceptor

--- src/main/java/custom_key/Application.java
+++ src/main/java/custom_key/Application.java
@@ -2,16 +2,23 @@ package custom_key;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 
 /**
  * Created by ryandesmond on 4/6/18.
  */
 @SpringBootApplication
-public class Application {//extends SpringBootServletInitializer {
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure (SpringApplicationBuilder application){
+        return application.sources(Application.class);
     }
 
 }
